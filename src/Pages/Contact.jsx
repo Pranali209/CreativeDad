a87b14ed179341ae7a47c91c7982743b938cfd3c
Pranali <pgmodgekar99@gmail.com>
2024-09-30 13:04:41 +0530

    contact page fix

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,7 +3,7 @@ import Header from '../Component/Header'
 import Button from '../Component/Button';
 
 
-function Contact({ showpopup, onClick  }) {
+function Contact({ showpopup, onClick }) {
     console.log(showpopup);
 
     return (
@@ -11,17 +11,20 @@ function Contact({ showpopup, onClick  }) {
             {
                 showpopup ? (
                     <>
-<div className="fixed inset-0 flex flex-col items-center justify-center
-                     z-50 bg-black  ">
-                            <Header className="w-full absolute top-0 left-0 " />
+                        <div className="fixed inset-0 flex flex-col items-center justify-center
+                     z-50 bg-black">
+                            <Header className="w-full absolute top-0 left-0" />
                             <p className='text-white text-[20px]'>Just Say Hi At</p>
-                            <a href={`mailto:hello@creativdad.com`} className='text-white  text-[30px]'>
-                                hello@<span className='text-gray-500  hover:text-white'>creativdad.com</span></a>
-                            <Button className="bg-white w-fit  mt-4
+                            <a href={`mailto:hello@creativdad.com`} className='text-white text-[30px]'>
+                                hello@<span className='text-gray-700 hover:text-white
+                                Email'>creativdad.com</span>
+                            </a>
+                            <Button className="bg-white w-fit mt-4
     text-black px-4 py-1 rounded-md transition-all duration-200 ease-in-out
-     hover:bg-gray-200 focus:outline-none focus:ring-2  hover:scale-75  active:shadow-inner"
-                                onClick={onClick} txt = "Go Back" /> 
+     hover:bg-gray-200 focus:outline-none focus:ring-2 hover:scale-75 active:shadow-inner"
+                                onClick={onClick} txt="Go Back" />
                         </div>
+
                     </>
                 ) : " "
             }
